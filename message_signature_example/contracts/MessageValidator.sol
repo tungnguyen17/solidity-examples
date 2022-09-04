34f54eef6749df58333bb0a62e01fea80875d738
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2022-09-05 01:01:39 +0700

    multi signatures version

--- message_signature_example/contracts/MessageValidator.sol
+++ message_signature_example/contracts/MessageValidator.sol
@@ -36,8 +36,46 @@ contract MessageValidator {
     return msg.sender == signer;
   }
 
+  function checkSignatures(
+    bytes32 dataHash_,
+    bytes memory signatures_,
+    address[] memory addresses_
+  ) public pure returns (uint256 result) {
+
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+    uint256 i;
+    result = 0;
+    for (i = 0; (i + 1) * 65 <= signatures_.length; i++) {
+      address signer;
+      (v, r, s) = signatureSplitN(signatures_, i);
+      if (v == 0) {
+        // If v is 0 then it is a contract signature
+        // Not supported
+      } else if (v == 1) {
+        // If v is 1 then it is an approved hash
+        // When handling approved hashes the address of the approver is encoded into r
+        signer = address(uint160(uint256(r)));
+        // Hashes are automatically approved by the sender of the message or when they have been pre-approved via a separate transaction
+      } else if (v > 30) {
+        // If v > 30 then default va (27,28) has been adjusted for eth_sign flow
+        // To support eth_sign and similar we adjust v and hash the messageHash with the Ethereum message prefix before applying ecrecover
+        signer = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", dataHash_)), v - 4, r, s);
+      } else {
+        // Default is the ecrecover flow with the provided data hash
+        // Use ecrecover with the messageHash for EOA signatures
+        signer = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", dataHash_)), v, r, s);
+      }
+      bool isMatch = signer == addresses_[i];
+      if(isMatch) {
+        ++result;
+      }
+    }
+    return result;
+  }
+
   /// @dev divides bytes signature into `uint8 v, bytes32 r, bytes32 s`.
-  /// @notice Make sure to perform a bounds check for @param pos, to avoid out of bounds access on @param signatures
   /// @param signature_ concatenated rsv signatures
   function signatureSplit(
     bytes memory signature_
@@ -59,6 +97,36 @@ contract MessageValidator {
       // 'byte' is not working due to the Solidity parser, so lets
       // use the second best option, 'and'
       v := and(mload(add(signature_, 0x41)), 0xff)
+    }
+  }
+
+  /// @dev divides bytes signature into `uint8 v, bytes32 r, bytes32 s`.
+  /// @notice Make sure to perform a bounds check for @param pos, to avoid out of bounds access on @param signatures
+  /// @param signatures_ concatenated rsv signatures
+  /// @param index_ which signature to read. A prior bounds check of this parameter should be performed, to avoid out of bounds access
+  function signatureSplitN(
+    bytes memory signatures_,
+    uint256 index_
+  ) internal pure returns (
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  )
+  {
+    // The signature format is a compact form of:
+    //   {bytes32 r}{bytes32 s}{uint8 v}
+    // Compact means, uint8 is not padded to 32 bytes.
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      let pos := mul(0x41, index_)
+      r := mload(add(signatures_, add(pos, 0x20)))
+      s := mload(add(signatures_, add(pos, 0x40)))
+      // Here we are loading the last 32 bytes, including 31 bytes
+      // of 's'. There is no 'mload8' to do this.
+      //
+      // 'byte' is not working due to the Solidity parser, so lets
+      // use the second best option, 'and'
+      v := and(mload(add(signatures_, add(pos, 0x41))), 0xff)
     }
   }
 
--- message_signature_example/tests/decode_multiple_signature.spec.ts
+++ message_signature_example/tests/decode_multiple_signature.spec.ts
@@ -5,8 +5,9 @@ import {
   MessageValidator__factory
 } from '../types';
 import { ethers } from 'ethers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
-describe('check_single_signature_test', function() {
+describe('check_multiple_signatures_test', function() {
 
   let MessageValidator: MessageValidator__factory;
   let messageValidatorAddress: string;
@@ -18,43 +19,36 @@ describe('check_single_signature_test', function() {
     messageValidatorAddress = messageValidator.address;
   })
 
-  it('wrong_signer', async function() {
-
-    const [signer1, signer2] = await hre.ethers.getSigners();
-
-    const param1: number = 256;
-
-    const hash = ethers.utils.solidityKeccak256(
-      ["uint256"],
-      [param1],
-    );
-    const hashBytes = ethers.utils.arrayify(hash);
-    const signature = await signer1.signMessage(hashBytes);
-
-    const messageValidator = MessageValidator.attach(messageValidatorAddress);
-    const isSuccess = await messageValidator.connect(signer2)
-      .checkSignature(
-        hashBytes,
-        signature,
-      );
-    expect(isSuccess).is.false;
-  })
-
   it('precomputed_hash_1', async function() {
+    const [signer] = await hre.ethers.getSigners();
     const messageValidator = MessageValidator.attach(messageValidatorAddress);
     await precomputedHash(
       messageValidator,
       ["uint256"],
       ["256"],
+      [signer],
     )
   })
 
   it('precomputed_hash_2', async function() {
+    const [signer1, signer2] = await hre.ethers.getSigners();
     const messageValidator = MessageValidator.attach(messageValidatorAddress);
     await precomputedHash(
       messageValidator,
       ["uint256", "address"],
       ["128", "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"],
+      [signer1, signer2],
+    )
+  })
+
+  it('precomputed_hash_3', async function() {
+    const [signer1, signer2, signer3] = await hre.ethers.getSigners();
+    const messageValidator = MessageValidator.attach(messageValidatorAddress);
+    await precomputedHash(
+      messageValidator,
+      ["uint256", "address"],
+      ["128", "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"],
+      [signer1, signer2, signer3],
     )
   })
 })
@@ -63,20 +57,26 @@ async function precomputedHash(
   messageValidator: MessageValidator,
   types: string[],
   params: any[],
+  signers: SignerWithAddress[],
 ) {
-  const [signer] = await hre.ethers.getSigners();
 
   const hash = ethers.utils.solidityKeccak256(
     types,
     params,
   );
   const hashBytes = ethers.utils.arrayify(hash);
-  const signature = await signer.signMessage(hashBytes);
+  const signatures = await Promise.all(signers.map(async signer => {
+    const signature = await signer.signMessage(hashBytes);
+    return signature.substring(2);
+  }));
+  const finalSignature = ethers.utils.arrayify('0x' + signatures.join(''));
 
-  const isSuccess = await messageValidator.connect(signer)
-    .checkSignature(
+  const addresses = signers.map(x => x.address);
+  const count = await messageValidator.connect(signers[0])
+    .checkSignatures(
       hashBytes,
-      signature,
+      finalSignature,
+      addresses,
     );
-  expect(isSuccess).is.true;
+  expect(count).equals(signers.length);
 }
